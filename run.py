2e08608a73674e7ecbfe088a90b4b97e5971d3a4
“Peteroche” <petergoddey08l@gmail.com>
2025-10-05 01:18:14 +0100

    Dockerfile formatting warnings

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import uvicorn
 from src.main import app # noqa: F401
 
 if __name__ == "__main__":
-    uvicorn.run("run:app", host="0.0.0.0", port=8000)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
 
 
